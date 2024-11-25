dad783d1f9a761e98fda8b12029d8e0ac3ab50fe
Abdullah Madi <abodmadi2040@gmail.com>
2024-11-25 20:32:59 +0200

    Change in payments and add roule

---
+++ prisma/migrations/20241125181956_updated_in_payments_and_create_enum_for_methods/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `paymentMethod` to the `payments` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `payments` ADD COLUMN `paymentMethod` ENUM('BankTransfer', 'ElectronicWallet') NOT NULL;
---
+++ prisma/migrations/20241125182259_updated_in_payments_for_change_posision/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `paymentMethod` on the `payments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `payments` DROP COLUMN `paymentMethod`;
